25ee9a44838dbca6f4bfdbf7c85ad3cbdf296b36
Nginika <nginikaugochukwu@gmail.com>
2023-08-08 14:07:05 +0000

    implement require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth():
     """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """checks if authorization is required"""
-        return False
+        check = path
+        if path is None:
+            return True
+        if excluded_paths is None or []:
+            return True
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """create header with authorization details"""
