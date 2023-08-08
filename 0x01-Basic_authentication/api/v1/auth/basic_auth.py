2a6264d0dd532f0369bd8e4038abefecd187004a
Nginika <nginikaugochukwu@gmail.com>
2023-08-08 17:21:34 +0000

    implement seperating the code from the header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 """the basic auth class"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """basic authorization methods"""
-    def __init__(self):
-        """empty class for now"""
-        pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extracts the authorization code"""
+        if (authorization_header is None or not
+                isinstance(authorization_header, str)):
+            return None
+        if authorization_header[0:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
