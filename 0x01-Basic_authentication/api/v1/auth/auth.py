9c8c882381ddf457c2337e3351e926927a807d5d
Nginika <nginikaugochukwu@gmail.com>
2023-08-08 13:30:11 +0000

    pycodestyle check

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,10 +9,8 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
 
-
     def authorization_header(self, request=None) -> str:
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         return None
