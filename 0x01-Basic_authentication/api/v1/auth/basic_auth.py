d79ea3b7c89cf5cf7b1569e096ce2b2509d27e7b
Nginika <nginikaugochukwu@gmail.com>
2023-08-08 16:16:34 +0000

    introduce basic auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""the basic auth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """basic authorization methods"""
+    def __init__(self):
+        """empty class for now"""
+        pass
