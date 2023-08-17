0785c42ac216e7a99240c677565c48c538aa6058
Nginika <nginikaugochukwu@gmail.com>
2023-08-17 09:45:25 +0000

    adjusted documentation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -10,12 +10,10 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """Database class"""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -23,8 +21,7 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -34,7 +31,9 @@ class DB:
         """Add a user to database"""
         if not email or not hashed_password:
             return
-        new_user = User(email=email, hashed_password=hashed_password)
+        new_user = User()
+        new_user.email = email
+        new_user.hashed_password = hashed_password
         self._session.add(new_user)
         self._session.commit()
         return new_user
