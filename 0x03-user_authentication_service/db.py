112af5dd72490fbddd3a84d3a76c2261b5cbfdfe
Nginika <nginikaugochukwu@gmail.com>
2023-08-17 09:59:32 +0000

    improve documentation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """DB module
+
+contains the class DB definition of functions and methods
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,10 +12,18 @@ from user import Base, User
 
 
 class DB:
-    """Database class"""
+    """
+    A Database class for managing user data.
+    This class provides methods for initializing the database,
+    adding users, and managing database sessions.
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
+        """Initialize a new DB instance.
+        Initializes the database engine, drops existing tables,
+        creates new tables,and initializes a session
+        for database operations.
+        """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -21,16 +31,27 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object.
+        get a session object for database operations.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to database"""
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
+        """
         if not email or not hashed_password:
-            return
+            raise ValueError("Both email and hashed_password are required.")
         new_user = User()
         new_user.email = email
         new_user.hashed_password = hashed_password
